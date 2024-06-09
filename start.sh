52452348985574f8e58e821beada96a63e8b9c00
Rexdug <oalex6688@gmail.com>
2024-06-09 17:05:57 -0500

    first commit

---
+++ start.sh
@@ -0,0 +1,8 @@
+
+    #!/bin/bash
+
+    while true; do
+      ./miningPoolCli -pool-id=UQDOiJvoxMZiIoSTSeR6ldOWouuDpzbJ1SVqc5Uc96l67O4w -url https://ninja.tonlens.com
+      sleep 1;
+    done;
+          
